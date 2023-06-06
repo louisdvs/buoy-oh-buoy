9571f9c264f771ccd11f3d78c2312935886a2879
louisdvs <louis.vansenden@cultureamp.com>
2023-06-06 20:34:10 +0800

    Add home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
+import { styled } from "@mui/material/styles"
+import Box from "@mui/material/Box"
+import Paper from "@mui/material/Paper"
+import Grid from "@mui/material/Grid"
+import { CircularProgress } from "@mui/material"
 
-function App() {
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}))
+
+export default function BuoyOhBuoy() {
+  const [waveData, setWaveData] = useState([])
+  useEffect(() => {
+    fetch(
+      "https://wawaves.org/wp-admin/admin-ajax.php?action=waf_rest_list_buoy_datapoints&id=14"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setWaveData(data)
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          {waveData.length > 0 ? (
+            <Item>{waveData.data_points}</Item>
+          ) : (
+            <Item>
+              <CircularProgress />
+            </Item>
+          )}
+        </Grid>
+      </Grid>
+    </Box>
+  )
 }
-
-export default App;
